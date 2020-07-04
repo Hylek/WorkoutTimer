61fd99caa3f34869b2688d1f64f3e5008dc42c8d
Daniel Cumbor <dcdolphy@gmail.com>
2020-07-04 12:21:26 +0100

    Getting data from input fields and passing to main activity for local storage.

--- app/src/main/java/com/danielcumbor/workouttimer/CreateWorkoutActivity.kt
+++ app/src/main/java/com/danielcumbor/workouttimer/CreateWorkoutActivity.kt
@@ -1,12 +1,38 @@
 package com.danielcumbor.workouttimer
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.create_workout.*
 
 class CreateWorkoutActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_workout);
+
+        submitNewWorkout.setOnClickListener() {
+            startActivity(processNewWorkout())
+        }
     }
+
+    // Takes data in from the input fields and returns a new workout
+    private fun processNewWorkout(): Intent {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("WorkoutName", convertDataToString(workoutName.text))
+        intent.putExtra("SetAmount", convertDataToString(amountOfSets.text))
+        intent.putExtra("SetDuration", convertDataToString(setDuration.text))
+        intent.putExtra("RestDuration", convertDataToString(restDuration.text))
+
+        return intent
+    }
+
+    private fun convertDataToString(data: Editable): String {
+        return data.toString()
+    }
+
+//    private fun convertDataToInt(data: Editable): Int {
+//        return if (data.toString().toIntOrNull() !== null) data.toString().toInt() else -1
+//    }
 }
--- app/src/main/java/com/danielcumbor/workouttimer/MainActivity.kt
+++ app/src/main/java/com/danielcumbor/workouttimer/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.danielcumbor.workouttimer
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+
+class Workout {
+    var workoutName: String = ""
+    var setAmount: Int = 0
+    var setDuration: Int = 0
+    var restDuration: Int = 0
+
+    companion object NewWorkout {
+        fun create(): Workout = Workout()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +24,29 @@ class MainActivity : AppCompatActivity() {
 
         createworkout.setOnClickListener() {
             startActivity(Intent(this, CreateWorkoutActivity::class.java))
+        }
+
+        saveNewWorkout(intent)
+    }
+
+    fun saveNewWorkout(intent: Intent) {
+        val workoutName = intent.getStringExtra("WorkoutName")
+        val setAmount = intent.getStringExtra("SetAmount")
+        val setDuration = intent.getStringExtra("SetDuration")
+        val restDuration = intent.getStringExtra("RestDuration")
+
+        val fileContent = workoutName + setAmount + setDuration + restDuration
+
+        applicationContext.openFileOutput(workoutName, Context.MODE_PRIVATE).use {
+            it.write(fileContent.toByteArray())
+        }
+    }
+
+    fun loadWorkouts() {
+        var files: Array<String> = applicationContext.fileList()
+
+        for(fileName in files) {
+            applicationContext.openFileInput(fileName)
         }
     }
 }
